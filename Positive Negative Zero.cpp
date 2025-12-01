bdf5097f3f63ed2eb6c298aa884bd4ef1b60b8a3
Anjoly <anjoliauw02@gmail.com>
2025-12-01 12:54:48 +0600

    Add.Create Positive Negative Zero.cpp

---
+++ Positive Negative Zero.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    if (num > 0) {
+        cout << "Positive\n";
+    }
+    else if (num < 0) {
+        cout << "Negative\n";
+    }
+    else {
+        cout << "Zero\n";
+    }
+
+    return 0;
+}
