572a1a5dd611d15ac8cde8d7f0e8acea53b24d02
Anjoly <anjoliauw02@gmail.com>
2025-12-01 12:59:48 +0600

    Add.Create Area of a Circle.cpp

---
+++ Area of a Circle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    float radius, area;
+    radius = 5.20;
+
+    area = 3.1416 * radius * radius;
+
+    cout << "The area is " << fixed << setprecision(3) << area;
+
+    return 0;
+}
