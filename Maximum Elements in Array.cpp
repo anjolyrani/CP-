64e1bc433bd25a6f0183f80bd7d4a0749945593b
Anjoly <anjoliauw02@gmail.com>
2025-12-02 21:43:35 +0600

    Add.Create Maximum Elements in Array.cpp

---
+++ Maximum Elements in Array.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int max_val = arr[0];
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > max_val) {
+            max_val = arr[i];
+        }
+    }
+
+    cout << "The maximum element in the array is: " << max_val << endl;
+
+    return 0;
+}
