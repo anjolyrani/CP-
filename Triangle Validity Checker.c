36578a822a745ea68ca3955414594863fd987494
Anjoly <anjoliauw02@gmail.com>
2025-12-02 21:47:32 +0600

    Add.Create Triangle Validity Checker.cpp

---
+++ Triangle Validity Checker.c
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int side1, side2, side3;
+
+    cout << "Enter three sides of triangle:\n";
+    cin >> side1 >> side2 >> side3;
+
+    if ((side1 + side2 > side3) &&
+        (side2 + side3 > side1) &&
+        (side1 + side3 > side2)) 
+    {
+        cout << "Triangle is valid.";
+    }
+    else {
+        cout << "Triangle is not valid.";
+    }
+
+    return 0;
+}
