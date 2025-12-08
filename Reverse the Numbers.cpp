11d173b278d19b930149fe6fac2eb59ab35f3c4f
Anjoly <anjoliauw02@gmail.com>
2025-12-08 22:08:31 +0600

    Add.Create Reverse the Numbers.cpp

---
+++ Reverse the Numbers.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while(T--) {
+        int N, rev = 0;
+        cin >> N;
+        while(N > 0) {
+            rev = rev * 10 + N % 10;
+            N /= 10;
+        }
+        cout << rev << "\n";
+    }
+}
